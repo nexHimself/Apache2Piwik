8abe6f837db0302b62ec9d3bb4103ca7e88dc2d1
Oliver Gerlich <oliver.gerlich@gmx.de>
2011-12-31 17:53:40 +0100

    add support for reading gzip-compressed log files

--- apache2piwik.py
+++ apache2piwik.py
@@ -25,6 +25,7 @@ import time
 from hashlib import md5
 import pygeoip
 from src.uasparser import UASparser  
+import gzip
 
 
 #############################################################################
@@ -707,7 +708,10 @@ def apache2piwik(d):
         print 'Started processing '+d+apache_log_file+' file...'
         tstart = datetime.now()
         try:
-            f = open(apache_log_file,'r')
+            if apache_log_file.endswith('.gz'):
+                f = gzip.open(apache_log_file,'r')
+            else:
+                f = open(apache_log_file,'r')
         except IOError, e:
             print str(e).replace('[Errno 2] ','')
             sys.exit(0)
